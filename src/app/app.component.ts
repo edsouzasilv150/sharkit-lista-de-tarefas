91dd7f2ef49641405b321b05992b7d1e0e52e647
Edilson Sousa da Silva <edsouzasilv@hotmail.com>
2024-05-25 10:14:17 -0300

    feat: created project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'sharkit-lista-de-tarefas';
+  title = 'Gerenciador de Tarefas';
 }
