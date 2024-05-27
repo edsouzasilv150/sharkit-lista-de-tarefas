8855864b7cc5dbc822c950e8369b4bb7a34c168f
Edilson Sousa da Silva <edsouzasilv@hotmail.com>
2024-05-27 20:23:13 -0300

    feat: created methods

--- src/app/tarefas/listar/listar.component.ts
+++ src/app/tarefas/listar/listar.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Tarefa } from '../shared/tarefa.model';
+import { TarefaService } from '../shared';
 
 @Component({
   selector: 'app-listar',
   templateUrl: './listar.component.html',
   styleUrls: ['./listar.component.scss']
 })
-export class ListarTarefaComponent {
+export class ListarTarefaComponent implements OnInit {
+  tarefas: Tarefa[] | undefined;
 
+  constructor(private tarefaService: TarefaService) {}
+
+  ngOnInit(): void {
+    this.listarTodos();
+  }
+
+  listarTodos(): void {
+    this.tarefaService.listarTodos().subscribe({
+      next: tarefas => this.tarefas = tarefas,
+      error: error => console.error('Erro ao listar tarefas', error),
+      complete: () => console.log('Lista completa')
+    });
+  }
+
+  remover($event: any, tarefa: Tarefa): void {
+    $event.preventDefault();
+    if (confirm(`Deseja remover a tarefa? ${tarefa.nome}?`)) {
+      if (tarefa.id !== undefined) {  // Verifique se tarefa.id não é undefined
+        this.tarefaService.remover(tarefa.id).subscribe({
+          next: () => this.listarTodos(),
+          error: error => console.error('Erro ao remover tarefa', error),
+          complete: () => console.log('Remoção completa')
+        });
+      } else {
+        console.error('ID da tarefa é undefined');
+      }
+    }
+  }
+
+  alterarStatus(tarefa: Tarefa): void {
+    if (confirm('Deseja alterar o status da tarefa "' + tarefa.nome + '"?')) {
+      if (tarefa.id !== undefined) {  // Verifique se tarefa.id não é undefined
+        this.tarefaService.remover(tarefa.id).subscribe({
+          next: () => this.listarTodos(),
+          error: error => console.error('Erro ao alterar status da tarefa', error),
+          complete: () => console.log('Remoção completa')
+        });
+      } else {
+        console.error('ID da tarefa é undefined');
+      }
+    }
+  }
 }
