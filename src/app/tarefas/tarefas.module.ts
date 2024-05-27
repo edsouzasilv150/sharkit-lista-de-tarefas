d8bddfceb159ee26047a888ad00356bea2b3395c
Edilson Sousa da Silva <edsouzasilv@hotmail.com>
2024-05-27 11:42:25 -0300

    feat: created components to list, to create and to edit.

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TarefaService } from './shared';
+import { ListarComponent } from './listar/listar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EditarComponent } from './editar/editar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListarComponent,
+    CadastrarComponent,
+    EditarComponent
+  ],
   imports: [
     CommonModule
   ],
