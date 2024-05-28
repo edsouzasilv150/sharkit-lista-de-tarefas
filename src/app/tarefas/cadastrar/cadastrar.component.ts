7e04999b9324643063296fb84fa83a44c573d6cf
Edilson Sousa da Silva <edsouzasilv@hotmail.com>
2024-05-28 09:15:09 -0300

    feat: created method register new task

--- src/app/tarefas/cadastrar/cadastrar.component.ts
+++ src/app/tarefas/cadastrar/cadastrar.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { TarefaService, Tarefa } from '../shared';
 
 @Component({
   selector: 'app-cadastrar',
   templateUrl: './cadastrar.component.html',
   styleUrls: ['./cadastrar.component.scss']
 })
-export class CadastrarTarefaComponent {
+export class CadastrarTarefaComponent implements OnInit {
 
+  @ViewChild('formTarefa') formTarefa!: NgForm;
+  tarefa: Tarefa = new Tarefa();
+
+  constructor(
+    private tarefaService: TarefaService,
+    private router: Router) {}
+
+  ngOnInit(): void {}
+
+  cadastrar(): void {
+    if (this.formTarefa.form.valid) {
+      this.tarefaService.cadastrar(this.tarefa).subscribe({
+        next: () => this.router.navigate(['/tarefas']),
+        error: error => console.error('Erro ao cadastrar tarefa', error)
+      });
+    }
+  }
 }
