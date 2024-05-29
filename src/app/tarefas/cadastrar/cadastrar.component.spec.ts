5bbed6f51d0fb0320669903a4774cd480e2f230a
Edilson Sousa da Silva <edsouzasilv@hotmail.com>
2024-05-29 13:45:01 -0300

    feat: created component, code and refacture

--- src/app/tarefas/cadastrar/cadastrar.component.spec.ts
+++ src/app/tarefas/cadastrar/cadastrar.component.spec.ts
@@ -1,23 +1,68 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed, fakeAsync, tick } from '@angular/core/testing';
 
+import { Tarefa, TarefaService } from '../shared';
 import { CadastrarTarefaComponent } from './cadastrar.component';
+import { Router } from '@angular/router';
+import { of, throwError } from 'rxjs';
+import {  FormsModule, NgForm } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
-describe('CadastrarComponent', () => {
+describe('CadastrarTarefaComponent', () => {
   let component: CadastrarTarefaComponent;
-  let fixture: ComponentFixture<CadastrarTarefaComponent>;
+  let tarefaService: jasmine.SpyObj<TarefaService>;
+  let router: jasmine.SpyObj<Router>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CadastrarTarefaComponent ]
-    })
-    .compileComponents();
+  beforeEach(() => {
+    const tarefaServiceSpy = jasmine.createSpyObj('TarefaService', ['cadastrar']);
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
 
-    fixture = TestBed.createComponent(CadastrarTarefaComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    TestBed.configureTestingModule({
+      imports: [FormsModule, RouterTestingModule],
+      providers: [
+        CadastrarTarefaComponent,
+        { provide: TarefaService, useValue: tarefaServiceSpy },
+        { provide: Router, useValue: routerSpy }
+      ]
+    });
+
+    component = TestBed.inject(CadastrarTarefaComponent);
+    tarefaService = TestBed.inject(TarefaService) as jasmine.SpyObj<TarefaService>;
+    router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
+
+    component.formTarefa = { form: { valid: true } } as any;
+    component.tarefa = new Tarefa();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('deve cadastrar a tarefa e navegar no cadastro bem-sucedido', fakeAsync(() => {
+    const mockTarefas: Tarefa[] = [component.tarefa];
+    tarefaService.cadastrar.and.returnValue(of(mockTarefas));
+
+    component.cadastrar();
+    tick();
+
+    expect(tarefaService.cadastrar).toHaveBeenCalledWith(component.tarefa);
+    expect(router.navigate).toHaveBeenCalledWith(['/listar']);
+  }));
+
+  it('deve registrar erro se o cadastro falhar', fakeAsync(() => {
+    const mockError = new Error('Erro ao cadastrar tarefa');
+    tarefaService.cadastrar.and.returnValue(throwError(() => mockError));
+    const consoleErrorSpy = spyOn(console, 'error');
+
+    component.cadastrar();
+    tick();
+
+    expect(tarefaService.cadastrar).toHaveBeenCalledWith(component.tarefa);
+    expect(router.navigate).not.toHaveBeenCalled();
+    expect(consoleErrorSpy).toHaveBeenCalledWith('Erro ao cadastrar tarefa', mockError);
+  }));
+
+  it('não deve cadastrar tarefa se o formulário for inválido', () => {
+    component.formTarefa = { form: { valid: false } } as NgForm;
+
+    component.cadastrar();
+
+    expect(tarefaService.cadastrar).not.toHaveBeenCalled();
+    expect(router.navigate).not.toHaveBeenCalled();
   });
 });
--- src/app/tarefas/cadastrar/cadastrar.component.ts
+++ src/app/tarefas/cadastrar/cadastrar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, NgForm, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { TarefaService, Tarefa } from '../shared';
 
@@ -9,20 +9,23 @@ import { TarefaService, Tarefa } from '../shared';
   styleUrls: ['./cadastrar.component.scss']
 })
 export class CadastrarTarefaComponent implements OnInit {
+  nome = new FormControl('', [Validators.required, Validators.minLength(5)]);
 
   @ViewChild('formTarefa') formTarefa!: NgForm;
-  tarefa: Tarefa = new Tarefa();
+  tarefa!: Tarefa;
 
   constructor(
     private tarefaService: TarefaService,
-    private router: Router) {}
+    private router: Router) {
+      this.tarefa = new Tarefa();
+    }
 
   ngOnInit(): void {}
 
   cadastrar(): void {
     if (this.formTarefa.form.valid) {
       this.tarefaService.cadastrar(this.tarefa).subscribe({
-        next: () => this.router.navigate(['/tarefas']),
+        next: () => this.router.navigate(['/listar']),
         error: error => console.error('Erro ao cadastrar tarefa', error)
       });
     }
