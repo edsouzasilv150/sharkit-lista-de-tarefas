956a8e2a60cc40586661dceb314cdfb08a1de949
Edilson Sousa da Silva <edsouzasilv@hotmail.com>
2024-05-29 13:38:51 -0300

    feat: config routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { LoginComponent } from './tarefas/login';
 import { TarefaRoutes } from './tarefas/tarefas-routing.module';
 
 const routes: Routes = [
+  { path: '', component: LoginComponent },
   {
-    path: '',
-    redirectTo: '/tarefas/listar',
-    pathMatch: 'full'
+    path: 'tarefas',
+    loadChildren: () => import('./tarefas/tarefas.module').then(m => m.TarefasModule)
   },
-   ...TarefaRoutes
+  ...TarefaRoutes
 ];
 
 @NgModule({
