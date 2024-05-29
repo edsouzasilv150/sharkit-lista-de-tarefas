65cb1c8f55f9b8e26d0f6b422b865325d26921e0
Edilson Sousa da Silva <edsouzasilv@hotmail.com>
2024-05-29 13:42:07 -0300

    add modules

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -1,23 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TarefaService } from './shared';
+import { TarefaService, TarefaConcluidaDirective } from './shared';
 import { ListarTarefaComponent } from './listar/listar.component';
 import { CadastrarTarefaComponent } from './cadastrar/cadastrar.component';
 import { EditarTarefaComponent } from './editar/editar.component';
-
-
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     ListarTarefaComponent,
     CadastrarTarefaComponent,
-    EditarTarefaComponent
+    EditarTarefaComponent,
+    TarefaConcluidaDirective,
   ],
   imports: [
     CommonModule,
+    RouterModule,
+    FormsModule
   ],
   providers: [
     TarefaService
-  ]
+  ],
 })
 export class TarefasModule { }
