e8826f6f79bf4aaeb269891cca50db68ba78e464
Edilson Sousa da Silva <edsouzasilv@hotmail.com>
2024-05-29 13:40:54 -0300

    add modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TarefasModule } from './tarefas';
+import { LoginComponent } from './tarefas/login';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    TarefasModule
+    TarefasModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
